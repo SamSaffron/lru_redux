4218cc39f1e0564cb77cc1b3efbd955800cc7409
Seberius <admin@infiniteskycorp.com>
2015-02-15 18:50:02 -0800

    ThreadSafeCache implementation changed for significant performance gain in .getset.

--- lib/lru_redux/thread_safe_cache.rb
+++ lib/lru_redux/thread_safe_cache.rb
@@ -1,22 +1,75 @@
-require 'thread'
 require 'monitor'
 
 class LruRedux::ThreadSafeCache < LruRedux::Cache
   include MonitorMixin
-  def initialize(size)
-    super(size)
+
+  def initialize(max_size)
+    super(max_size)
   end
 
-  def self.synchronize(*methods)
-    methods.each do |method|
-      define_method method do |*args, &blk|
-        synchronize do
-          super(*args,&blk)
-        end
-      end
+  def max_size=(size)
+    synchronize do
+      super(size)
     end
   end
 
-  synchronize :[], :[]=, :each, :to_a, :delete, :count, :valid?, :max_size, :fetch, :getset
+  def getset(key)
+    synchronize do
+      super(key)
+    end
+  end
 
+  def fetch(key)
+    synchronize do
+      super(key)
+    end
+  end
+
+  def [](key)
+    synchronize do
+      super(key)
+    end
+  end
+
+  def []=(key, value)
+    synchronize do
+      super(key, value)
+    end
+  end
+
+  def each
+    synchronize do
+      super
+    end
+  end
+
+  def to_a
+    synchronize do
+      super
+    end
+  end
+
+  def delete(key)
+    synchronize do
+      super(key)
+    end
+  end
+
+  def clear
+    synchronize do
+      super
+    end
+  end
+
+  def count
+    synchronize do
+      super
+    end
+  end
+
+  def valid?
+    synchronize do
+      super
+    end
+  end
 end
